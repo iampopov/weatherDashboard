b83930456bc6766285ab73b50f724d0920a7ebb0
IamPopov <ivantsar@gmail.com>
2019-12-23 18:29:40 -0700

    building up js

---
+++ script.js
@@ -0,0 +1,4 @@
+var cityArr = [];
+locationCity = "";
+var APIKey = "31deb4a0aa0fd513e099894690e4c592";
+var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${locationCity}&appid=${APIKey}&units=metric`;
