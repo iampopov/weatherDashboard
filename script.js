7ffa146505605d8f6af22f773a4fed17f45b162f
IamPopov <ivantsar@gmail.com>
2019-12-23 18:31:29 -0700

    added my api key

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 var cityArr = [];
 locationCity = "";
-var APIKey = "31deb4a0aa0fd513e099894690e4c592";
+var APIKey = "cda6d992003316bb25ecdc1ba9f95bcc"; //"31deb4a0aa0fd513e099894690e4c592";
 var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${locationCity}&appid=${APIKey}&units=metric`;
