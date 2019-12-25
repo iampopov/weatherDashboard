c4cada36dc59eae1dc17fd21b97d7f6c9fd02d03
IamPopov <ivantsar@gmail.com>
2019-12-24 18:13:31 -0700

    storing the array of cities logic developed in local storage

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-var cityArr = [];
-console.log(cityArr);
 
+//console.log(cityArr);
+cityArr = [];
+console.log(cityArr);
 locationCity = "";
 var APIKey = "cda6d992003316bb25ecdc1ba9f95bcc"; //"31deb4a0aa0fd513e099894690e4c592";
 var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${locationCity}&appid=${APIKey}&units=metric`;
@@ -10,9 +11,7 @@ var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${locationCity
 $(".btn-primary").on("click", function (e) {
     e.preventDefault();
     var newCity = $(".form-control").val().trim(); //grabbing input from text box
-    
-    //cityArr.push(newCity); //add it to the array
-    
+    cityArr.push(newCity);
+    localStorage.setItem('cities', JSON.stringify(cityArr));
     $(".form-control").val(""); // clear out search form for next entry
-    
 });
