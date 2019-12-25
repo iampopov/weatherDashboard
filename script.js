816a6457fd0f8bb597a4985712ebf083b08aa505
IamPopov <ivantsar@gmail.com>
2019-12-24 18:25:08 -0700

    getting values from local storage and converting back into array

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 
 //console.log(cityArr);
 cityArr = [];
-console.log(cityArr);
 locationCity = "";
 var APIKey = "cda6d992003316bb25ecdc1ba9f95bcc"; //"31deb4a0aa0fd513e099894690e4c592";
 var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${locationCity}&appid=${APIKey}&units=metric`;
 
+function renderButtons () {
+var renderCities = JSON.parse(localStorage.getItem('cities')); //getting values stored in local storage and converting them back into array
 
+
+
+}
 
 $(".btn-primary").on("click", function (e) {
     e.preventDefault();
@@ -14,4 +18,6 @@ $(".btn-primary").on("click", function (e) {
     cityArr.push(newCity);
     localStorage.setItem('cities', JSON.stringify(cityArr));
     $(".form-control").val(""); // clear out search form for next entry
+    renderButtons();
 });
+renderButtons();
